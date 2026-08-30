0984028031eed3aebbaa31edee37c3b2cbb0e7dc
yushangcan <2551520580@qq.com>
2026-08-30 23:14:51 +0800

    fix: update redis admission script test

--- internal/redisstore/store_test.go
+++ internal/redisstore/store_test.go
@@ -1,10 +1,13 @@
 package redisstore
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestAdmissionScriptContainsAtomicSteps(t *testing.T) {
 	for _, part := range []string{"SISMEMBER", "HINCRBY", "request_id", "SOLD_OUT", "ACCEPTED"} {
-		if !stringsContains(admissionScript, part) {
+		if !strings.Contains(admissionScript, part) {
 			t.Fatalf("admission script is missing %q", part)
 		}
 	}
